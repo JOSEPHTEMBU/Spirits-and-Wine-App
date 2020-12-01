c89b5c6d73245673d642d994611322cf80ee0932
JOSEPHTEMBU <josephmulama30@gmail.com>
2020-12-01 13:27:06 +0300

    added menu photo xml file

--- app/src/main/java/com/example/spiritsandwineapp/Constants.java
+++ app/src/main/java/com/example/spiritsandwineapp/Constants.java
@@ -5,6 +5,7 @@ import com.example.spiritsandwineapp.BuildConfig;
 public class Constants {
     public static final String BREW_BASE_API = "https://api.punkapi.com/v2/";
     public static final String FIREBASE_QUERY_INDEX = "index";
+
   // public static final String PUNK_API_KEY = BuildConfig.PUNK_API_KEY;
 //    public static final String PREFERENCES_LOCATION_KEY = "location";
 }
