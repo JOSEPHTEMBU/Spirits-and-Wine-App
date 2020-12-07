629cd187fd45d1074b09b5be5e5ea66fab1b8a4b
JOSEPHTEMBU <josephmulama30@gmail.com>
2020-12-07 20:48:29 +0300

    Added animation

---
+++ app/src/main/java/com/example/spiritsandwineapp/adapters/FirebaseSpiritsAndWineViewHolder.java
@@ -0,0 +1,13 @@
+package com.example.spiritsandwineapp.adapters;
+
+import android.animation.AnimatorInflater;
+import android.animation.AnimatorSet;
+
+import com.example.spiritsandwineapp.R;
+
+public class FirebaseSpiritsAndWineViewHolder {
+
+
+
+
+}
